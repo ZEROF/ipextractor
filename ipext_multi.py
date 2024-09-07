ea20742ff146fc708220e444fe923ea8dd6ebd39
Zerof <security@netmajstor.com>
2024-09-08 01:35:09 +0200

    Update Crowdsec endpoint

--- ipext_multi.py
+++ ipext_multi.py
@@ -36,7 +36,7 @@ urls = [
     'https://www.binarydefense.com/banlist.txt',
     'https://raw.githubusercontent.com/UoFruitE/ProcessedLists/main/processed_dshield.txt',
     'https://opendbl.net/lists/blocklistde-all.list',
-    'https://opendbl.root.rodeo/security/blacklist.txt',
+    'https://opendbl.root.rodeo/security/blocklist',
     'https://opendbl.net/lists/talos.list',
     'https://opendbl.net/lists/bruteforce.list'
 ]
